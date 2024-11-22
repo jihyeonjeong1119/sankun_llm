1d102c5bbfd2c3548e9a4d714ca23b1790652f50
junhkang <junhkang91@gmail.com>
2024-11-22 14:30:20 +0900

    - type 		feat 		fix 		docs 		style 		refactor 		test 		chore 	- subject

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_validate_question_success():
     question = "서울시 아파트 공사 현장은 어떤게 있을까?";
     is_valid, reason = validate_question(question);
-    assert is_valid == True   
+    assert is_valid == True      
 #
 # def test_validate_question_failure():
 #     response = client.post("/questions/validate", json={"text": ""})
